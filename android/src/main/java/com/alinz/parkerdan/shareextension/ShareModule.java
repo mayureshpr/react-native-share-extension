c13ffd9c908c7ee8ead64d8c320b5342a463861a
Alessandro <andonealessandro@outlook.it>
2019-11-07 18:59:29 +0100

    Update ShareModule.java

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -12,7 +12,10 @@ import android.content.Intent;
 import android.net.Uri;
 
 import android.graphics.Bitmap;
+import android.util.Log;
+
 import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class ShareModule extends ReactContextBaseJavaModule {
@@ -47,25 +50,41 @@ public class ShareModule extends ReactContextBaseJavaModule {
       Activity currentActivity = getCurrentActivity();
 
       if (currentActivity != null) {
+
         Intent intent = currentActivity.getIntent();
         action = intent.getAction();
         type = intent.getType();
+
         if (type == null) {
           type = "";
         }
+
         if (Intent.ACTION_SEND.equals(action) && "text/plain".equals(type)) {
           value = intent.getStringExtra(Intent.EXTRA_TEXT);
-        }
-        else if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/gif".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) ) {
+
+        } else if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/gif".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) ) {
           Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-          if(uri.toString().contains("data:image/")){
-            value = uri.toString();
-          } else {
-            value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
-          }
-       } else {
+          value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
+       } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+
+            if (type.startsWith("image/")) {
+                ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+                ArrayList<String> images = new ArrayList<String>();
+
+                if (imageUris != null) {
+                    for(int i = 0; i < (imageUris.size() > 5 ? 5 : imageUris.size()) ; i++){
+                     //   Log.d("IMAGE - " , "file://" + RealPathUtil.getRealPathFromURI(currentActivity, imageUris.get(i)));
+                        images.add("file://" + RealPathUtil.getRealPathFromURI(currentActivity, imageUris.get(i)));
+                    }
+                   // Log.d("ARRALIST - " , images.toString());
+                    value = images.toString();
+                }
+            }
+
+        } else {
          value = "";
        }
+
       } else {
         value = "";
         type = "";
